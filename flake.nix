2635475c83dcf0dea6b4304ff5dd3c2567e8e0e1
annabella-bananabella <annabellaf1019@gmail.com>
2026-02-25 04:22:23 +0100

    Create a basic flake.nix
    The flake is very barebones. The only inputs are flake-parts which
    allows for flake modularization and nixpkgs which is required for
    practically everything. All nix files in modules/ are imported, these
    will contain flake-parts modules.
    
    Even though it is currently empty I had to make a modules/ directory
    to avoid an from referring to a nonexistent path. I made a .gitkeep
    file so it could be tracked by git, which is necessary for the flake to
    acknowledge the directory's existence.

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    disko.url = "github:nix-community/disko";
+
+  };
+
+  outputs =
+    inputs@{
+      flake-parts,
+      nixpkgs,
+      ...
+    }:
+
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      top@{
+        config,
+        withSystem,
+        moduleWithSystem,
+        ...
+      }:
+      let
+
+        listNixFilesInDir =
+          directory: with nixpkgs.lib.fileset; fileFilter (file: file.hasExt "nix") directory |> toList;
+
+      in
+      {
+
+        systems = [ "x86_64-linux" ];
+
+        imports = listNixFilesInDir ./modules;
+
+      }
+    );
+}
