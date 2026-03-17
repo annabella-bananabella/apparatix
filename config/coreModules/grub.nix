345062a5a28d55e123b891b04e05a5f53f2c818d
annabella-bananabella <annabellaf1019@gmail.com>
2026-03-17 02:50:26 +0100

    Use grub as a bootloader
    The grub bootloader with very basic configuration has now been
    implemented. Later it will need to be further configured.

---
+++ config/coreModules/grub.nix
@@ -0,0 +1,22 @@
+{
+  inputs,
+  nixpkgs,
+  lib,
+  ...
+}:
+{
+
+  nixpkgs.boot.loader = {
+
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi";
+    };
+
+    grub = {
+      efiSupport = true;
+      device = "nodev";
+    };
+  };
+
+}
